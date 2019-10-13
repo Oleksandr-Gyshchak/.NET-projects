f0103ef24fbe4971bbd26a9571756cc20a6f608d
Aleksandr Gyshchak <sasha991@ukr.net>
2019-10-13 22:54:43 +0300

    Home work 9

---
+++ Csharp STARTER/HomeWork9/HomeWork9/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string msgEmployeeCount = "Введите количество сотрудников предприятия ";
+
+            int employeeCount = 0;
+            double[,] databaseArr;
+
+            Console.WriteLine(msgEmployeeCount);
+
+            employeeCount = int.Parse(Console.ReadLine());
+
+            // 1-я строка отвечает за стоимость трудового часа сотрудника
+            // 2-я за количество отработанных сотрудником часов в отчетном периоде
+            databaseArr = new double[2, employeeCount];
+
+            for (int i = 0; i < employeeCount; i++)
+            {
+                AddEmployee(i, ref databaseArr);
+            }
+
+
+            DisplayEmployeeSalaryByIndex(ref databaseArr, employeeCount);
+
+            DisplayAllEmployeeSalary(ref databaseArr, employeeCount);
+
+            Console.ReadKey();
+        }
+
+        static void AddEmployee(int employeeIndex, ref double[,] databaseArr)
+        {
+            string msgHourlyRate = "Введите стоимость 1 трудового часа сотрудника  №";
+            string msgAmountOfHoursWorked = "Введите количество отработанных часов для сотрудника №";
+
+            int employeeNumber = employeeIndex + 1;
+            double hourlyRate = 0;
+            int amountHoursWorked = 0;
+
+
+            Console.WriteLine(msgHourlyRate + employeeNumber);
+            hourlyRate = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(msgAmountOfHoursWorked + employeeNumber);
+            amountHoursWorked = int.Parse(Console.ReadLine());
+
+
+            databaseArr[0, employeeIndex] = hourlyRate;
+            databaseArr[1, employeeIndex] = amountHoursWorked;
+        }
+
+        static void DisplayEmployeeSalaryByIndex(ref double[,] databaseArr, int employeeCount)
+        {
+            string msgInputEmployeeId = "Введите № сотрудника от 0 ";
+            string msgSalary = "Объем заработной платы сотрудника № ";
+            string invalidEmployeeId = "Вы ввели ID несуществующего сотрудника";
+
+            Console.WriteLine($"{msgInputEmployeeId} до {employeeCount - 1}");
+            int employeeIndex = int.Parse(Console.ReadLine());
+
+            try
+            {
+                double total = total = databaseArr[0, employeeIndex] * databaseArr[1, employeeIndex];
+                Console.WriteLine($"{msgSalary}{employeeIndex} : {total}");
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine($"{invalidEmployeeId} : {employeeIndex}");
+            }
+
+        }
+
+        static void DisplayAllEmployeeSalary(ref double[,] databaseArr, int employeeCount)
+        {
+            string msgTotal = "Общий объем бюджета заработной платы за отчетный период ";
+
+            double total = 0;
+
+            for (int i = 0; i < employeeCount; i++)
+            {
+                total += databaseArr[0, i] * databaseArr[1, i];
+            }
+
+            Console.WriteLine($"{msgTotal} : {total}");
+        }
+    }
+}
